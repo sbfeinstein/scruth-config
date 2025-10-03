923dd82ddd2ead02535d6b4b8b201f704c459696
Scott Feinstein <Scott_Feinstein@yahoo.com>
2025-10-02 22:18:48 -0400

    feat: initial chezmoi support
    temporary usage of branch for chezmoi init
    
    fix: quote the things in the toml
    
    fix ssh files maybe
    
    fix: another attempt
    
    fix: space in exports
    
    fix: brewfile location
    
    fix: ssh stuff
    
    fix: ssh file permissions
    
    iterate on git and setup script
    
    fix git config gpg properties
    
    fix git signing config
    
    test signed commit
    
    fix more attempts to fix git signing
    
    test signed commit 2
    
    test signed commit 2
    
    fix git signing
    
    fix file permissions
    
    fix file permissions
    
    fix allowed_signers
    
    format git config template
    
    add IntelliJ and a gitignore
    
    feat: oh-my-zshell
    
    feat: chez refinements
    
    feat: chezmoi iteration and prep for main branch
    
    fix: review feedback

---
+++ setup.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -euo pipefail
+
+_check_machine_is_scruthsystem() {
+  is_allowed_scruthsystem() {
+    ALLOWED_HOSTNAMES=("sfeinstein-studio-m4")
+    hostname=$(scutil --get ComputerName)
+    for allowed_hostname in "${ALLOWED_HOSTNAMES[@]}"; do
+      if [[ "$hostname" == "$allowed_hostname" ]]; then
+        return 0
+      fi
+    done
+    return 1
+  }
+
+  hostname=$(scutil --get ComputerName)
+  if is_allowed_scruthsystem; then
+    echo "🚀 Setting up ScruthSystem™️  $hostname"
+  else
+    echo "❌  This system ('$hostname') is not an allowed ScruthSystem™️  , aborting setup"
+    exit 1
+  fi
+}
+
+_check_machine_is_scruthsystem
+
+# Xcode command line tools are a prerequisite for Homebrew.
+# So we install them independently rather than manage them via brew.
+if xcode-select -p &>/dev/null; then
+  echo "✅  Xcode command line tools are already installed"
+else
+  echo -n "🔧  Installing Xcode command line tools..."
+  xcode-select --install &>/dev/null
+  
+  while ! xcode-select -p &>/dev/null; do
+    # echo a single dot on the same line
+    echo -n "."
+    sleep 5
+  done
+  echo "✅  Xcode command line tools installed successfully"
+fi
+
+if which -s "brew"; then
+  echo "✅  Homebrew is already installed"
+else
+  echo "🍺  Installing Homebrew"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "✅  Homebrew installed successfully"
+fi
+
+if which -s "op"; then
+  echo "✅  1Password CLI is already installed"
+else
+  echo "🔐  Installing 1Password CLI"
+  brew install --cask 1password
+  brew install --cask 1password-cli
+  echo "✅  1Password and its CLI installed successfully"
+fi
+
+if which -s "chezmoi"; then
+  echo "✅  Chezmoi is already installed"
+else
+  echo "🛠️  Installing Chezmoi"
+  brew install chezmoi
+  echo "✅  Chezmoi installed successfully"
+fi
+
+# Sign in to 1Password
+while ! op whoami &>/dev/null; do
+  echo "🔐  1Password CLI is not logged in or session expired..."
+  echo "    Manually installing desktop app integration may make it easier to sign-in:"
+  echo "    https://developer.1password.com/docs/cli/get-started#step-2-turn-on-the-1password-desktop-app-integration"
+  eval "$(op signin)"
+done
+echo "✅  1Password CLI is logged in"
+
+if [ -d "$(chezmoi source-path 2>/dev/null)" ]; then
+  echo "ℹ️  Chezmoi already initialized, pulling latest changes..."
+  chezmoi update
+  echo "✅  Chezmoi updated"
+else
+  chezmoi init sbfeinstein/scruth-config --branch main
+  chezmoi apply
+  echo "✅  Chezmoi initialized"
+fi
+
+echo "😎  Finished setting up $hostname"
