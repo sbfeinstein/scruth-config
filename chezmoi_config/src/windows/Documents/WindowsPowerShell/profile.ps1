bcf24277e84e51b745200a908952041695a40609
Scott Feinstein <Scott_Feinstein@yahoo.com>
2026-03-10 10:35:43 -0400

    feat: add st alias

--- chezmoi_config/src/windows/Documents/WindowsPowerShell/profile.ps1
+++ chezmoi_config/src/windows/Documents/WindowsPowerShell/profile.ps1
@@ -1,3 +1,6 @@
 # Common functionality across all versions of PowerShell
 function Get-GitStatus { git status $args }
 Set-Alias -Name gst -Value Get-GitStatus
+
+function Open-SublimeFile { subl $args }
+Set-Alias -Name st -Value Open-SublimeFile
