10f6c9943e72589f3c38d3e1a36771b6c1e0c467
Scott Feinstein <Scott_Feinstein@yahoo.com>
2026-09-05 15:30:19 -0400

    feat: add scruth backup
    Invokes backup via task scheduler.
    Also adds logging for the backup job in general (standard out) to /utilities/scruth-backup/backup.log

--- chezmoi_config/src/windows/dot_scruth-config/scruth.ps1
+++ chezmoi_config/src/windows/dot_scruth-config/scruth.ps1
@@ -1,14 +1,39 @@
 <#
 .SYNOPSIS
-    Manages machine configurations via chezmoi.
+    Manages machine configuration and related helpful commands
 #>
+[CmdletBinding()]
 param(
-    [string]$Action
+    [Parameter(Position = 0)]
+    [ValidateSet("up", "backup", "help", "-h", "--help", "/?")]
+    [string]$Action,
+
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$ExtraArgs
 )
 
-if ($Action -eq "up") {
-    & chezmoi apply --override-data '{\"upgrade_software\": \"true\"}'
-}
-else {
-    & chezmoi apply
+switch ($Action) {
+    "up" {
+        & chezmoi apply --override-data '{\"upgrade_software\": \"true\"}' @ExtraArgs
+    }
+    "backup" {
+        Start-ScheduledTask -TaskName "Scott tasks\scruth-backup"
+        Get-Content -Path "C:\Logs\MyTaskOutput.log" -Wait -Tail 10
+    }
+    { $_ -in "help", "-h", "--help", "/?" } {
+        @'
+Usage: scruth [<action>] [<args>]
+
+Actions:
+  up        Update machine state using chezmoi and install available winget upgrades
+  backup    Runs scruth-backup using Task Scheduler
+  help      Show this usage message
+
+Default:
+  Running 'scruth' with no action just updates bachine state using chezmoi and checks for available winget upgrades
+'@
+    }
+    Default {
+        & chezmoi apply @ExtraArgs
+    }
 }
